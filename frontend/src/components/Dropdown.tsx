36b40d407196c126a99765dc7cce7a5f219e96a2
JatinMehta007 <mehtajatin045@gmail.com>
2024-09-09 18:58:24 +0530

    changes

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -32,7 +32,8 @@ export const Dropdown =()=>{
           <div className="absolute top-16 right-8 bg-white  shadow-md rounded-md z-[100]">
             <div className="p-3">
 
-            <button className="flex w-full cursor-pointer  items-center gap-2 rounded-md pt-[9px] pb-2 px-4 font-sans text-sm antialiased font-normal  text-inherit hover:text-slate-500" onClick={Profile}>
+            <button className="flex w-full cursor-pointer  items-center gap-2 rounded-md pt-[9px] pb-2 px-4 font-sans text-sm antialiased font-normal  text-inherit hover:text-slate-500"
+             onClick = { Profile } >
 
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
